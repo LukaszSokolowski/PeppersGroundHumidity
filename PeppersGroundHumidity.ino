13499998ce19c15eb571fe6fcbdf3853b04a293b
Łukasz Sokołowski <lukasz.m.sokolowski1995@gmail.com>
2017-06-11 23:46:50 +0200

    hint for correct connection

--- PeppersGroundHumidity.ino
+++ PeppersGroundHumidity.ino
@@ -40,7 +40,7 @@
 #include <LiquidCrystal.h>
 
 // initialize the library with the numbers of the interface pins
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2); // plug it in corrently to arduino platform!
 
 void setup() {
   lcd.begin(16, 2);   // set up the LCD's number of columns and rows:
